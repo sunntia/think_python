4ffe98c42f2f8fe302c798d0d4dea349d1b4f6b4
Tatiana <123217546+sunntia@users.noreply.github.com>
2024-02-24 23:54:50 +0300

    Update ch1ex123.py

--- ch1ex123.py
+++ ch1ex123.py
@@ -8,3 +8,12 @@ mil_per_sec2 = (mil_per_min - 6) * 60
 print('Time per mile in seconds', mil_per_sec)
 print('Time per mile in minutes', mil_per_min)
 print('Time per mile in minutes and seconds:', int(mil_per_min),'minutes', int(mil_per_sec2), 'seconds')
+
+
+#What is your average speed in miles per hour?
+
+miles = 10 / 1.61
+seconds = 42 + (42 * 60)
+hours = seconds / 3600
+mil_per_hour = miles / hours
+print(mil_per_hour)
