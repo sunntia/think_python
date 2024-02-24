13156273dbccb1e2264b9e77d6dac712d2cee86f
Tatiana <123217546+sunntia@users.noreply.github.com>
2024-02-25 00:00:44 +0300

    Update ch1ex1_2.py

--- ch1ex1_2.py
+++ ch1ex1_2.py
@@ -1,6 +1,10 @@
+# How many seconds are there in 42 minutes 42 seconds?
+print(42+ (42 * 60))
+
+#How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile.
+print(10 / 1.61)
 
 #If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per mile in minutes and seconds)?
-
 miles = 10 / 1.61
 seconds = 42 + (42 * 60)
 mil_per_sec = seconds / miles
@@ -12,7 +16,6 @@ print('Time per mile in minutes and seconds:', int(mil_per_min),'minutes', int(m
 
 
 #What is your average speed in miles per hour?
-
 miles = 10 / 1.61
 seconds = 42 + (42 * 60)
 hours = seconds / 3600
