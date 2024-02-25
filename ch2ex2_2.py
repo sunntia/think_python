3868c7b1f332234ba2d84cd2a49b3d4da361d214
Tatiana <123217546+sunntia@users.noreply.github.com>
2024-02-25 19:23:19 +0300

    Create ch2ex2_2.py

---
+++ ch2ex2_2.py
@@ -0,0 +1,5 @@
+#The volume of a sphere with radius r is 4/3πr³. What is the volume of a sphere with radius 5?
+import math
+r = 5
+V = 4/3*math.pi*r**3
+print('The volume of a sphere with radius 5 is', V)
