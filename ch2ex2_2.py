1b4ae623309806142c14d39a84db92e5b9048cf0
Tatiana <123217546+sunntia@users.noreply.github.com>
2024-02-25 22:36:13 +0300

    Update ch2ex2_2.py

--- ch2ex2_2.py
+++ ch2ex2_2.py
@@ -15,3 +15,11 @@ book_cost = new_price * copies
 shipping_cost = shipping + (next_shipping * (copies - 1))
 total_cost = book_cost + shipping_cost
 print('The total wholesale cost for 60 copies is: $', total_cost)
+
+#3. If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile), then 3 miles at tempo (7:12 per mile) and 1 mile at easy pace again, what time do I get home for breakfast?
+start_time = (6*60 + 52)*60
+easy_time = (8*60 + 15)*2
+tempo_time = (7*60 + 12)*3
+breakfast_hour = (start_time + easy_time + tempo_time)/(60*60)
+breakfast_minute  = (breakfast_hour - int(breakfast_hour))*60
+print('Time returned: ', int(breakfast_hour), ':', int(breakfast_minute))
